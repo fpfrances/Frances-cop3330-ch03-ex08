e7c7cd94ebedecaa10985c67735bed64bdac6538
fpfrances <fpfrances@hotmail.com>
2021-11-22 21:05:10 -0500

    Add project files.

---
+++ Frances-cop3330-ch03-ex08.cpp
@@ -0,0 +1,14 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Filipe Frances
+ */
+#include "Header.h"
+
+int main(){
+   std::cout << "Please enter an integer: ";
+   std::cin >> value;
+     if (value % divisByTwo == equalToZero)
+       std::cout << "\n The value " << value << " is an even number." << std::endl;
+    else std::cout << "\n The value " << value << " is an odd number." << std::endl;
+    system("pause>0");
+}
---
+++ Header.h
@@ -0,0 +1,9 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Filipe Frances
+ */
+#pragma once
+#include <iostream>
+int value;
+int divisByTwo = 2;
+int equalToZero = 0;
